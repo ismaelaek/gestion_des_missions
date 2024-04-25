fa44da05315b139a70e2ad56e48adbfbb1e15e35
ismaelaek <ismael.aitelkamel17@gmail.com>
2024-04-25 16:23:39 +0100

    update

--- frontend/src/components/missionItem.jsx
+++ frontend/src/components/missionItem.jsx
@@ -11,7 +11,23 @@ const MissionItem = ({ object, etats, profs, juridections }) => {
     const jurid = juridections.find(
 		(jurid) => jurid.id === object.idJuridiction
 	);
-
+	let statusColor;
+	switch (object.idEtatMission) {
+		case 1:
+			statusColor = "bg-red-200"; 
+			break;
+		case 2:
+			statusColor = "bg-yellow-200"; 
+			break;
+		case 3:
+			statusColor = "bg-green-200";
+			break;
+		case 4:
+			statusColor = "bg-gray-200"; 
+			break;
+		default:
+			statusColor = "bg-gray-200"; 
+	}
 	const handleDelete = () => {
 		dispatch(deleteMission(object.id));
 	};
@@ -47,15 +63,20 @@ const MissionItem = ({ object, etats, profs, juridections }) => {
 					</button>
 				</Popconfirm>
 			</td>
-			<td className=" text-center">{jurid ? jurid.JurLibelle_ar : "N/A"}</td>
+			<td className=" text-center text-xs">
+				{jurid ? jurid.JurLibelle_ar : "N/A"}
+			</td>
 			{/* <td class="overflow-hidden">
 				<span class="content">{jurid ? jurid.JurLibelle_ar : "N/A"}</span>
 			</td> */}
-			<td>{etat ? etat.EtatLibelle_ar : "N/A"}</td>
+			<td className="flex items-center justify-end gap-2">
+				{etat ? etat.EtatLibelle_ar : "N/A"}
+				<div className={`${statusColor} w-2 h-2 rounded`}></div>
+			</td>
 			<td>{professionnel ? professionnel.NumeroSomme : "N/A"}</td>
 			{/* <td>{object.DateEdition}</td> */}
-			<td>{object.DateRetour}</td>
-			<td>{object.DateAller}</td>
+			<td className="text-xs">{object.DateRetour}</td>
+			<td className="text-xs">{object.DateAller}</td>
 			<td>{object.TypeMission}</td>
 			<td>{object.NummeroMission}</td>
 		</tr>
