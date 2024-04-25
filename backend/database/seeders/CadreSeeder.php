576b2bbbb781ed67e5db8e1814140e0db5258122
ismaelaek <ismael.aitelkamel17@gmail.com>
2024-04-25 16:24:07 +0100

    update

--- backend/database/seeders/CadreSeeder.php
+++ backend/database/seeders/CadreSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CadreSeeder extends Seeder
 {
@@ -12,6 +13,24 @@ class CadreSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'cadreLibelle_ar' => 'كاتب الضبط',
+                'created_at' => '2024-04-19 12:40:22',
+                'updated_at' => '2024-04-19 12:40:22',
+            ],
+            [
+                'cadreLibelle_ar' => 'محرر قضائي',
+                'created_at' => '2024-04-19 12:40:22',
+                'updated_at' => '2024-04-19 12:40:22',
+            ],
+            [
+                'cadreLibelle_ar' => 'منتدب قضائي',
+                'created_at' => '2024-04-19 12:40:22',
+                'updated_at' => '2024-04-19 12:40:22',
+            ],
+        ];
+
+        DB::table('cadres')->insert($data);
     }
 }
